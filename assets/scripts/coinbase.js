ae8243ad218cc9a0fa4f1b21253355dcf8a5efd0
Andrew McInally <realfunnyestate@yahoo.com>
2021-12-04 10:32:28 -0700

    added anchor tag

--- assets/scripts/coinbase.js
+++ assets/scripts/coinbase.js
@@ -1,165 +1,148 @@
 // target input form
-let formEl = $('#coin-search');
-let coin = '';
+let formEl = $("#coin-search");
+let coin = "";
 let amount;
 // target coin display id
-let coinDisplayEl = $('#coin-display');
-let userInputEl = $('#coin-name');
+let coinDisplayEl = $("#coin-display");
+let userInputEl = $("#coin-name");
 let myCoins = [];
-let myCoinsEl = $('#myCoins')
+let myCoinsEl = $("#myCoins");
 let storedCoins;
 
-loadCoins()
+loadCoins();
 
 function loadCoins() {
-    myCoinsEl.text('');
-    storedCoins = JSON.parse(localStorage.getItem("coins"));
-    if (storedCoins) {
-        for (let i = 0; i < storedCoins.length; i++) {
-            let coinBtn = $(
-                `<div class="row">
-                    <div col s12>
-                        <button id="${i}" data-coin="${storedCoins[i]}" class="myCoinBtn waves-effect waves-light btn-large">${storedCoins[i]}</button> <a id="${storedCoins[i]}" class="remove-coin-btn">Remove</a>
-                    </div>
-                </div>`
-            );
-            myCoinsEl.append(coinBtn);
-        }
+  myCoinsEl.text("");
+  storedCoins = JSON.parse(localStorage.getItem("coins"));
+  if (storedCoins) {
+    for (let i = 0; i < storedCoins.length; i++) {
+      let coinBtn = $(
+        `<div class="row">
+            <div col s12>
+                <button id="${i}" data-coin="${storedCoins[i]}" class="myCoinBtn waves-effect waves-light btn-large">${storedCoins[i]}</button> <a id="${storedCoins[i]}" class="remove-coin-btn">Remove</a>
+            </div>
+        </div>`
+      );
+      myCoinsEl.append(coinBtn);
     }
+  }
 }
 
 async function coins() {
-    await fetch('https://api.exchange.coinbase.com/currencies')
-        .then(response => {
-            return response.json();
-
-        })
-        .then(data => {
-            let allCoins = []
-            for (let i = 0; i < data.length; i++) {
-                //el.append(data[i].name + ' ' + data[i].id + '<br>')
-                allCoins.push(data[i].id)
-            }
-            $(function() {
-                $("#coin-input").autocomplete({
-                    source: allCoins
-                });
-            })
-        })
-        .catch(err => {
-            console.error(err);
-        })
+  await fetch("https://api.exchange.coinbase.com/currencies")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      let allCoins = [];
+      for (let i = 0; i < data.length; i++) {
+        allCoins.push(data[i].id);
+      }
+      $(function () {
+        $("#coin-input").autocomplete({
+          source: allCoins,
+        });
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
 
 coins();
 
 function formHandler(event) {
-    event.preventDefault();
-    // check to see if the submit came from a button or the input form
-    // if a button get the data attribute, q else get it from the form
-    if (formEl.attr('data-coin')) {
-        coin = formEl.attr('data-coin')
-            // clear the button attribute so it will not persist if the 
-            // next search is from the form.
-        formEl.attr('data-coin', '');
-    } else {
-        coin = $('input[name="coin-input"]').val().trim();
-    }
+  event.preventDefault();
+  // check to see if the submit came from a button or the input form
+  // if a button get the data attribute, q else get it from the form
+  if (formEl.attr("data-coin")) {
+    coin = formEl.attr("data-coin");
+    // clear the button attribute so it will not persist if the
+    // next search is from the form.
+    formEl.attr("data-coin", "");
+  } else {
+    coin = $('input[name="coin-input"]').val().trim();
+  }
 
-    //coin = $('input[name="coin-input"]').val();
-    let requestedUrl = `https://api.coinbase.com/v2/prices/${coin}-usd/sell`
+  let requestedUrl = `https://api.coinbase.com/v2/prices/${coin}-usd/sell`;
 
+  myCoins = [];
+  loadCoins();
+  coinbase();
+  async function coinbase() {
+    await fetch(requestedUrl)
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        storedCoins = JSON.parse(localStorage.getItem("coins"));
+        // Get saved searches from local storage
 
-    //localStorage.setItem("coins", JSON.stringify(myCoins));
-    myCoins = [];
-    loadCoins();
-    coinbase();
-    async function coinbase() {
-        await fetch(requestedUrl)
-            .then(response => {
-                return response.json();
-            })
-            .then(data => {
-                //if (!data.errors) {
-                //console.error(err , "error");
-                storedCoins = JSON.parse(localStorage.getItem("coins"));
-                // Get saved searches from local storage
+        // Check if there are any coins in local storage
 
-                // Check if there are any coins in local storage
-                console.log(myCoins);
-                if (storedCoins !== null) {
-                    myCoins = storedCoins
-                    console.log(myCoins);
-                    if (!myCoins.includes(coin)) {
-                        if (!data.errors) {
-                            myCoins.push(coin);
-                        }
-                        localStorage.setItem("coins", JSON.stringify(myCoins));
-                    }
-                } else {
-                    if (!data.errors) {
-                        myCoins.push(coin);
-                        localStorage.setItem("coins", JSON.stringify(myCoins));
-                    }
-                    //console.log(myCoins);
-                }
-                // console.log(coin);
-                //console.log(myCoins);
+        if (storedCoins !== null) {
+          myCoins = storedCoins;
 
+          if (!myCoins.includes(coin)) {
+            if (!data.errors) {
+              myCoins.push(coin);
+            }
+            localStorage.setItem("coins", JSON.stringify(myCoins));
+          }
+        } else {
+          if (!data.errors) {
+            myCoins.push(coin);
+            localStorage.setItem("coins", JSON.stringify(myCoins));
+          }
+        }
 
+        coinDisplayEl.text("");
+        amount = data.data.amount;
+        userInputEl.text(coin.toUpperCase());
+        coinDisplayEl.append("$" + Number(amount).toLocaleString());
+        // scroll to coin display for small screens
+        location.href = "#coin-name";
+        randomDrink();
 
-                coinDisplayEl.text('');
-                amount = data.data.amount;
-                userInputEl.text(coin.toUpperCase())
-                coinDisplayEl.append("$" + Number(amount).toLocaleString());
-                // scroll to coin display for small screens
-                location.href = "#coin-name"
-                randomDrink();
+        $('input[name="coin-input"]').val("");
 
-                $('input[name="coin-input"]').val('');
-
-                // } else {
-                //     return;
-                //}
-                loadCoins()
-                myCoins = '';
-            })
-            .catch(err => {
-                let drinkNameEl = $('#drinkName');
-                let drinkImgEl = $('#drinkImg');
-                drinkImgEl.empty();
-                drinkNameEl.empty();
-                userInputEl.empty();
-                coinDisplayEl.empty();
-                $('input[name="coin-input"]').val('');
-                location.href = "#coin-name"
-                console.error(err, "error");
-                if (!coin) {
-                    coinDisplayEl.append('<h3 class="red-text">Input Required</h3>')
-                } else {
-                    coinDisplayEl.append('<h3 class="red-text">Invalid Ticker</h3>')
-                }
-            })
-    }
-}
-formEl.on('submit', formHandler)
-
-// click handler for history buttons
-myCoinsEl.on('click', function(event) {
-    let btnClass = $(event.target).attr("class");
-    
-    if (btnClass === 'remove-coin-btn') {
-        let coinToRemove = $(event.target).attr("id");
-        myCoins = storedCoins = JSON.parse(localStorage.getItem("coins"));
-        console.log(myCoins);
-        myCoins = arrayRemove(myCoins, coinToRemove);
-        console.log(myCoins);
-        localStorage.setItem("coins", JSON.stringify(myCoins));
         loadCoins();
-    } else {
-        let coin = event.target.getAttribute('data-coin');
-        event.stopPropagation();
-        formEl.attr('data-coin', coin);
-        formEl.submit();
-    }
-})
+        myCoins = "";
+      })
+      .catch((err) => {
+        let drinkNameEl = $("#drinkName");
+        let drinkImgEl = $("#drinkImg");
+        drinkImgEl.empty();
+        drinkNameEl.empty();
+        userInputEl.empty();
+        coinDisplayEl.empty();
+        $('input[name="coin-input"]').val("");
+        location.href = "#coin-name";
+        console.error(err, "error");
+        if (!coin) {
+          coinDisplayEl.append('<h3 class="red-text">Input Required</h3>');
+        } else {
+          coinDisplayEl.append('<h3 class="red-text">Invalid Ticker</h3>');
+        }
+      });
+  }
+}
+formEl.on("submit", formHandler);
+
+// click handler for coin buttons
+myCoinsEl.on("click", function (event) {
+  let btnClass = $(event.target).attr("class");
+
+  if (btnClass === "remove-coin-btn") {
+    let coinToRemove = $(event.target).attr("id");
+    myCoins = storedCoins = JSON.parse(localStorage.getItem("coins"));
+    myCoins = arrayRemove(myCoins, coinToRemove);
+    localStorage.setItem("coins", JSON.stringify(myCoins));
+    loadCoins();
+  } else {
+    let coin = event.target.getAttribute("data-coin");
+    event.stopPropagation();
+    formEl.attr("data-coin", coin);
+    formEl.submit();
+  }
+});
